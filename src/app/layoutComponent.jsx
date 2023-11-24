861cb7bf9f7adb221c2b01baedd73fdd85e59a41
amiribnjaman <amiribnjaman@gmail.com>
2023-11-24 23:45:26 +0600

    Create post card simple designed

--- src/app/layoutComponent.jsx
+++ src/app/layoutComponent.jsx
@@ -4,7 +4,6 @@ import { Inter } from "next/font/google";
 import TopNavbar from "@/components/topNavbar";
 import LeftSidebar from "@/components/leftSidebar";
 import { usePathname } from "next/navigation";
-import React, { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +24,7 @@ export default function LayoutComponent({ children }) {
         </>
       )}
 
-      <div className="mt-4 w-[95%]  mx-auto flex gap-6">
+      <div className="mt-4 w-[95%] relative mx-auto flex gap-6">
         <div className=" w-1/4"></div>
         {children}
         <div className="w-1/4"></div>
--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -1,27 +1,39 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Feed() {
+  const [createPostCard, setCreatePostCard] = useState(true);
   return (
     <>
       {/*=============== CREATE POST SECTION================ */}
-      <div className="bg-white w-[90%] mx-auto px-2 py-3 shadow border rounded-md">
+      <div
+        className={`${
+          createPostCard ? "backdrop-blur-md" : ""
+        } bg-white w-[90%] mx-auto px-2 py-3 shadow border rounded-md`}
+      >
         <div className="mx-3 ">
           <div className="flex gap-2 justify-between mb-3">
             <div className="w-12 h-12 rounded-full bg-gray-100"></div>
-            {/*------------- POST FORM-------------- */}
-            <div className="w-[85%]">
-              <form action="" className="flex items-center">
-                <textarea
-                  name=""
-                  id=""
-                  cols="55"
-                  rows=""
-                  placeholder="Whats on your mind?"
-                  className="bg-[#F0F2F5] px-3 rounded-full"
-                ></textarea>
-              </form>
+            <div className="w-[87%]">
+              <button className="bg-[#F0F2F5] text-[17px] hover:bg-[#E4E6E9] text-[#606266] w-full text-left px-4 py-2 px-3 rounded-full">
+                What's on your mind? Mr. X
+              </button>
+
+              {/*----------------CREATE POST CARD------------------ */}
+              <div className="shadow-md border py-4 rounded-md w-[500px] fixed top-2/5 left-[2%] z-50 bg-white">
+                <h2 className="text-center text-2xl font-semibold pb-2">
+                  Create Post
+                </h2>
+                <hr />
+                {/*-----------USER--------- */}
+                <div className="px-4 py-2 flex gap-2">
+                  <div className="w-10 h-10 rounded-full bg-gray-100"></div>
+                  <h4 className="font-semibold">Mr. X</h4>
+
+                </div>
+              </div>
             </div>
           </div>
           <hr />
