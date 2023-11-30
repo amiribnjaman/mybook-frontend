67459c49a7c6e47d271bdbe346bb2a58142e2be3
amiribnjaman <amiribnjaman@gmail.com>
2023-11-30 14:09:17 +0600

    Notification push logic implemented

--- src/utilitis/SERVER_URL.js
+++ src/utilitis/SERVER_URL.js
@@ -1,2 +1,5 @@
 // SERVER/BACKEND HOSTING URL
-export const SERVER_URL = "https://mybook-wa0r.onrender.com/api";
+export const SERVER_URL = "http://localhost:4000/api";
+
+
+// https://mybook-wa0r.onrender.com
