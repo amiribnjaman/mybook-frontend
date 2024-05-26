60909877bd3379241bab44fd19536348a9a4eaee
amiribnjaman <amiribnjaman@gmail.com>
2024-05-27 02:56:14 +0600

    Signup page on testing mode!!

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -46,115 +46,167 @@ export default function SignupPage() {
   const customId = "custom-id-yes";
 
   return (
-    <div className="text-center w-full mt-8 w-full">
-      <h1 className="text-[40px] text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 from-10% via-sky-400 via-40% to-emerald-500 to-60%">
-        {/* 00A400 */}
-        Mybook
-      </h1>
-      <h5 className="text-[13px] text-slate-600">
-        We serve love. We connect with your beloved one.
-      </h5>
-      <div className="w-[60%] mx-auto mt-3 mb-6 py-3 ">
-        {/* <h3 className="text-2xl font-semibold"> Welcome to Mybook </h3> */}
+    <div className="">
+      {/* MASS BG */}
+      <div className="absolute">
+        <div className="absolute inset-0 justify-center">
+          <div className="bg-shape1 bg-primary opcity-50 bg-blur"></div>
+          <div className="bg-shape2 bg-teal opcity-50 bg-blur"></div>
+          <div className="bg-shape3 bg-purple opcity-50 bg-blur"></div>
+        </div>
+      </div>
+      <div className="w-full mt-20 flex gap-6">
+        <div className="w-[50%]">
+          <h1>
+            <span className="text-[40px] font-bold text-transparent bg-clip-text bg-gradient-to-l from-indigo-500 from-10% to-emerald-500 to-90%">
+              Mybook
+            </span>
+            {/* 00A400 */}
+          </h1>
+          <h5 className="text-[13px] text-slate-600">
+            We serve love. We connect with your beloved one.
+          </h5>
+          <div className="mt-2 mb-6 py-3 ">
+            {/* <h3 className="text-2xl font-semibold"> Welcome to Mybook </h3> */}
 
-        <div className="mx-6">
-          <form onSubmit={handleSubmit(signUpSubmit)} className="mt-1">
-            {/* Firstname field */}
-            <input
-              {...register("firstName", { required: true })}
-              type="text"
-              className="border rounded-md p-2 block w-full mt-3"
-              placeholder="Firstname"
-            />
-            {/* Firstname error */}
-            <p className="hidden">
-              {errors?.firstName &&
-                toast.error("First Name field is required", {
-                  toastId: customId,
-                })}
-            </p>
+            <div className="md:mr-6">
+              <form onSubmit={handleSubmit(signUpSubmit)} className="mt-1">
+                {/* Firstname field */}
+                <input
+                  {...register("firstName", { required: true })}
+                  type="text"
+                  className="border rounded-md p-2 block w-full mt-3"
+                  placeholder="Firstname"
+                />
+                {/* Firstname error */}
+                <p className="hidden">
+                  {errors?.firstName &&
+                    toast.error("First Name field is required", {
+                      toastId: customId,
+                    })}
+                </p>
 
-            {/* Surname field */}
-            <input
-              {...register("surName", { required: true })}
-              type="text"
-              className="border rounded-md p-2 block w-full mt-3"
-              placeholder="Surname"
-            />
-            {/* Surname Error */}
-            <p className="hidden">
-              {errors?.surName &&
-                toast.error("Surname Name field is required", {
-                  toastId: customId,
-                })}
-            </p>
+                {/* Surname field */}
+                <input
+                  {...register("surName", { required: true })}
+                  type="text"
+                  className="border rounded-md p-2 block w-full mt-2"
+                  placeholder="Surname"
+                />
+                {/* Surname Error */}
+                <p className="hidden">
+                  {errors?.surName &&
+                    toast.error("Surname Name field is required", {
+                      toastId: customId,
+                    })}
+                </p>
 
-            {/* Email field */}
-            <input
-              {...register("email", {
-                required: true,
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                },
-              })}
-              type="text"
-              className="border rounded-md p-2 block w-full mt-3"
-              placeholder="Email Address"
-            />
-            {/* Email errors */}
-            <p className="hidden">
-              {errors.email?.type === "pattern" &&
-                toast.error(
-                  `Invalid email. Please provide a valid email address.`,
-                  {
-                    toastId: customId,
-                  }
-                )}
-            </p>
-            <p className="hidden">
-              {errors?.email &&
-                toast.error("Email field is required", { toastId: customId })}
-            </p>
+                {/* Email field */}
+                <input
+                  {...register("email", {
+                    required: true,
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    },
+                  })}
+                  type="text"
+                  className="border rounded-md p-2 block w-full mt-2"
+                  placeholder="Email Address"
+                />
+                {/* Email errors */}
+                <p className="hidden">
+                  {errors.email?.type === "pattern" &&
+                    toast.error(
+                      `Invalid email. Please provide a valid email address.`,
+                      {
+                        toastId: customId,
+                      }
+                    )}
+                </p>
+                <p className="hidden">
+                  {errors?.email &&
+                    toast.error("Email field is required", {
+                      toastId: customId,
+                    })}
+                </p>
 
-            {/* Password field */}
-            <input
-              {...register("password", { required: true, minLength: 6 })}
-              type="password"
-              className="border rounded-md p-2 block w-full mt-3"
-              placeholder="New Password"
-            />
-            {/* Password errors */}
-            <p className="hidden">
-              {errors.password?.type === "minLength" &&
-                toast.error(
-                  `Password is too short. 
+                {/* Password field */}
+                <input
+                  {...register("password", { required: true, minLength: 6 })}
+                  type="password"
+                  className="border rounded-md p-2 block w-full mt-2"
+                  placeholder="New Password"
+                />
+                {/* Password errors */}
+                <p className="hidden">
+                  {errors.password?.type === "minLength" &&
+                    toast.error(
+                      `Password is too short. 
                 Please provide atleast 6 characters.`,
-                  {
-                    toastId: customId,
-                  }
-                )}
-            </p>
-            <p className="hidden">
-              {errors?.password &&
-                toast.error("Password field is required", {
-                  toastId: customId,
-                })}
-            </p>
+                      {
+                        toastId: customId,
+                      }
+                    )}
+                </p>
+                <p className="hidden">
+                  {errors?.password &&
+                    toast.error("Password field is required", {
+                      toastId: customId,
+                    })}
+                </p>
 
-            <button
-              type="submit"
-              className="bg-gradient-to-r from-green-400 to-blue-500 hover:bg-gradient-to-l  text-white text-lg font-semibold rounded-md px-8 py-2 mt-6 mb-3 w-full"
-            >
-              Signup
-            </button>
-          </form>
-          <p>or</p>
-          <Link
-            href="/login"
-            className="text-dark text-[14px] underline font-semibold"
-          >
-            Already have an account?
-          </Link>
+                <button
+                  type="submit"
+                  className="bg-gradient-to-r from-green-400 to-blue-500 hover:bg-gradient-to-l text-white text-lg font-semibold rounded-md px-8 py-2 mt-6 mb-3 w-full"
+                >
+                  Signup
+                </button>
+              </form>
+              <p className="text-[15px]">
+                or
+                <Link
+                  href="/login"
+                  className="text-dark text-[15px] ml-1 underline font-semibold"
+                >
+                  Already have an account?
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/*RIGHTSIDE*/}
+        <div className="SIGNUP_RIGHT_SIDE w-[50%]">
+          <div className="text-left">
+            <h1 className="text-md font-semibold mt-10">
+              About
+              <span className="text-green-500 ml-1">
+                Mybook
+              </span>
+            </h1>
+            <p className="text-[14px] mb-4">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries,
+            </p>
+            <h2 className="font-semibold">Features</h2>
+            <ul className="list-disc text-[14px] ml-4 mt-1">
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </li>
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard
+              </li>
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
