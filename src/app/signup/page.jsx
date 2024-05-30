f9d3bf5014dce5248e20babb27495aedde678db6
amiribnjaman <amiribnjaman@gmail.com>
2024-05-30 14:55:55 +0600

    Signup page redesign

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -186,6 +186,7 @@ export default function SignupPage() {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries,
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, doloremque.
             </p>
             <h2 className="font-semibold">Features</h2>
             <ul className="list-disc text-[14px] ml-4 mt-1">
