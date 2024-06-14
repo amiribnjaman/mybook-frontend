f2fac9830c9dae03228021eeb3de58ddba677891
amiribnjaman <amiribnjaman@gmail.com>
2024-06-15 02:42:15 +0600

    User profile section update

--- src/components/rightSidebar.jsx
+++ src/components/rightSidebar.jsx
@@ -194,7 +194,7 @@ export default function RightSidebar() {
           <div
             className={`${
               showNotificationCard
-                ? "absolute top-[60px] right-[-50px] py-4 px-1 border bg-white shadow-md w-[250px] rounded-md h-auto"
+                ? "absolute top-[60px] right-[-50px] py-4 px-1 border bg-white shadow-md w-[250px] rounded-md h-auto z-[50] text-[15px]"
                 : "hidden"
             }`}
           >
@@ -202,7 +202,7 @@ export default function RightSidebar() {
               showNotification()?.likeUnread?.map((notification, index) => (
                 <p
                   key={index}
-                  className="text-[13px] font-semibold cursor-pointer px-6 py-1 rounded-md my-[5px] block text-black bg-blue-50"
+                  className="text-[15px] font-semibold cursor-pointer px-6 py-1 rounded-md my-[5px] block text-black bg-blue-50"
                 >
                   <span className="font-semibold pr-1">
                     {notification?.count}
@@ -273,17 +273,58 @@ export default function RightSidebar() {
               }`}
             >
               <div className="flex flex-col gap-2 text-left text-[16px]">
-                <Link href="" className="font-semibold mb-2">
-                  Profile
+                <Link
+                  href=""
+                  className="font-semibold mb-2 flex justify-between items-center"
+                >
+                  <span>Profile</span>
+                  <svg
+                    className="w-5 h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 640 512"
+                  >
+                    <path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM609.3 512H471.4c5.4-9.4 8.6-20.3 8.6-32v-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2h61.4C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z" />
+                  </svg>
                 </Link>
-                <Link href="" className="font-semibold mb-2">
-                  Friends
+                <Link
+                  href=""
+                  className="font-semibold mb-2 flex justify-between items-center"
+                >
+                  <span>Friends</span>
+                  <svg
+                    className="w-5 h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 640 512"
+                  >
+                    <path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM609.3 512H471.4c5.4-9.4 8.6-20.3 8.6-32v-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2h61.4C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z" />
+                  </svg>
                 </Link>
-                <Link href="" className="font-semibold mb-2">
-                  Groups
+                <Link
+                  href=""
+                  className="font-semibold mb-2 flex justify-between items-center"
+                >
+                  <span>Pages</span>
+                  <svg
+                    className="w-5 h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 384 512"
+                  >
+                    <path d="M320 464c8.8 0 16-7.2 16-16V160H256c-17.7 0-32-14.3-32-32V48H64c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320zM0 64C0 28.7 28.7 0 64 0H229.5c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64z" />
+                  </svg>
                 </Link>
-                <Link href="" className="font-semibold mb-2">
-                  Pages
+
+                <Link
+                  href=""
+                  className="font-semibold mb-2 flex justify-between items-center"
+                >
+                  <span>Groups</span>
+                  <svg
+                    className="w-5 h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 640 512"
+                  >
+                    <path d="M72 88a56 56 0 1 1 112 0A56 56 0 1 1 72 88zM64 245.7C54 256.9 48 271.8 48 288s6 31.1 16 42.3V245.7zm144.4-49.3C178.7 222.7 160 261.2 160 304c0 34.3 12 65.8 32 90.5V416c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V389.2C26.2 371.2 0 332.7 0 288c0-61.9 50.1-112 112-112h32c24 0 46.2 7.5 64.4 20.3zM448 416V394.5c20-24.7 32-56.2 32-90.5c0-42.8-18.7-81.3-48.4-107.7C449.8 183.5 472 176 496 176h32c61.9 0 112 50.1 112 112c0 44.7-26.2 83.2-64 101.2V416c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32zm8-328a56 56 0 1 1 112 0A56 56 0 1 1 456 88zM576 245.7v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM320 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM240 304c0 16.2 6 31 16 42.3V261.7c-10 11.3-16 26.1-16 42.3zm144-42.3v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM448 304c0 44.7-26.2 83.2-64 101.2V448c0 17.7-14.3 32-32 32H288c-17.7 0-32-14.3-32-32V405.2c-37.8-18-64-56.5-64-101.2c0-61.9 50.1-112 112-112h32c61.9 0 112 50.1 112 112z" />
+                  </svg>
                 </Link>
                 <Link href="" className="font-semibold mb-2">
                   Help & Support
@@ -304,7 +345,7 @@ export default function RightSidebar() {
         </div>
       </div>
 
-      {/*--------------Right Profile nav menu-------------- */}
+      {/*--------------Right Profile Completion Card-------------- */}
       <div className="mt-6 mb-4 px-2 py-3 rounded border">
         <h3 className="text-[16px] font-semibold">Complete your Profile</h3>
         <div className="mt-4 text-center">
@@ -323,28 +364,32 @@ export default function RightSidebar() {
                   <span className="bg-white w-4 h-4 rounded-full border border-slate-400 inline-block"></span>
                   General Information
                 </h5>
-                <h6 className="text-[#a5ddc6]">4/6</h6>
+                <h6 className="text-slate-800">4/6</h6>
               </div>
               <div className="flex justify-between">
                 <h5 className="flex gap-2 items-center">
                   <span className="bg-white w-4 h-4 rounded-full border border-slate-400 inline-block"></span>
                   Work Experience
                 </h5>
-                <h6 className="text-[#a5ddc6]">1/3</h6>
+                <h6 className="text-slate-800">1/3</h6>
               </div>
               <div className="flex justify-between">
                 <h5 className="flex gap-2 items-center">
-                  <span className="bg-[#a5ddc6] w-4 h-4 rounded-full inline-block"></span>
+                  <span className="bg-[#53cf9b] w-4 h-4 rounded-full inline-block text-[10px] flex items-center justify-center font-semibold text-white">
+                    &#10003;
+                  </span>
                   Profile Photo
                 </h5>
-                <h6 className="text-[#a5ddc6]">1/1</h6>
+                <h6 className="text-[#53cf9b]">1/1</h6>
               </div>
               <div className="flex justify-between">
                 <h5 className="flex gap-2 items-center">
-                  <span className="bg-[#a5ddc6] w-4 h-4 rounded-full inline-block"></span>
+                  <span className="bg-[#53cf9b] w-4 h-4 rounded-full inline-block text-[10px] flex items-center justify-center font-semibold text-white">
+                    &#10003;
+                  </span>
                   Cover Photo
                 </h5>
-                <h6 className="text-[#a5ddc6]">1/1</h6>
+                <h6 className="text-[#53cf9b]">1/1</h6>
               </div>
               {/* <Steps
               style={{ color: "#20BA59" }}
